f0bd869bc61de17978eeb5de112659fd3931b8e5
TEE-EMM97 <tevmc@live.com>
2022-12-27 19:28:51 +0000

    feat: adding graphql

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use crate::routes::health;
-use axum::{routing::get, Router, Server};
+use crate::model::QueryRoot;
+use crate::routes::{graphql_handler, graphql_playground, health};
+use async_graphql::{EmptyMutation, EmptySubscription, Schema};
+use axum::{extract::Extension, routing::get, Router, Server};
+mod model;
 mod routes;
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/health", get(health));
+    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription).finish();
+    let app = Router::new()
+        .route("/", get(graphql_playground).post(graphql_handler))
+        .route("/health", get(health))
+        .layer(Extension(schema)); // (1)
     Server::bind(&"0.0.0.0:8080".parse().unwrap())
         .serve(app.into_make_service())
         .await
